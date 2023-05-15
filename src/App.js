245768faf5c46a11c9c97634759d95387b773c9a
mannShrestha01 <130300983+mannShrestha01@users.noreply.github.com>
2023-05-15 10:08:48 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Content from './Content'
+import Footer from './Footer';
+function App() {
+  //js code write inside the function
+  
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
